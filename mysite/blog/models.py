b78e7f6dc7857006af8f2f30c7d76b659752eb71
Denis Budin <D20041152@yandex.ru>
2023-11-05 04:23:21 +0300

    the blog model is ready

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,15 +1,26 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Post(models.Model):  # модель данных для постов блога
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)  # короткая метка
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,  # поведение при удалении
+                               related_name='blog_posts')  # взаимосвязь М-О
     body = models.TextField()  # тело поста
     publish = models.DateTimeField(default=timezone.now)  # дата и время публикации поста
     created = models.DateTimeField(auto_now_add=True)  # дата и время создания поста
     updated = models.DateTimeField(auto_now=True)  # последняя дата и время обновления поста
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
 
     class Meta:
         ordering = ['-publish']  # обратная сортировка по полю publish
