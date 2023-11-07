e7f5b95686a360fe75075c7358eb619e0c04b53e
Denis Budin <D20041152@yandex.ru>
2023-11-07 20:18:54 +0300

    created an adminustration site for the Post model

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish',
+                    'status']  # задаёт поля модели, которые показываются на странице админ. модели
+    list_filter = ['status', 'created', 'publish', 'author'] # показывает сбоку фильтры
+    search_fields = ['title', 'body'] # поля, по которым идет поиск, появляется поле поиска
+    prepopulated_fields = {'slug': ('title',)} # автоматически заполняет slug по title
+    raw_id_fields = ['author'] # более приемлемое отображание для выбора автора
+    date_hierarchy = 'publish' # показывает иерархию дат(под поиском)
+    ordering = ['status', 'publish'] # сортирует при нажатии на кнопку (критерии сортировки)
