a064773a45ae5f6c1b3373bd080518b8e132d2dc
Denis Budin <D20041152@yandex.ru>
2024-03-21 03:07:35 +0300

    create form send message

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
+
+class PostListView(ListView):
+    """
+    Альтернатиыное представление списка постов
+    """
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
+
+    
 
 def post_list(request):
     post_list = Post.published.all()
@@ -27,3 +41,33 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
         
     return render(request, "blog/post/detail.html", {"post": post})
+
+def post_share(request, post_id):
+    #Извлечь пост по идентификатору id
+    post = get_object_or_404(Post, 
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    
+    sent = False
+    
+    if request.method == "POST":
+        #Форма была передана на обработку
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # Поля формы успешно прошли валидацию
+            cd = form.cleaned_data
+
+            post_url = request.build_absolute_uri( # используем, чтобы сформировать полный url-адрес(uri), включая HTTP и хост
+                post.get_absolute_url())
+            subject = f"{cd["name"]} recommends you read {post.title}"
+            message = f"Read {post.title} at {post_url}\n\n {cd["name"]}'s comments: {cd["comments"]}"
+            send_mail(subject, message, "primpyto@gmail.com", [cd["to"]])
+            sent = True
+            
+            # ... отправить электронное письмо
+    else:
+        form = EmailPostForm()
+    return render(request, "blog/post/share.html", {"post": post, 
+                                                    "form": form,
+                                                    "sent": sent})
+
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "primpyto@gmail.com"
+EMAIL_HOST_PASSWORD = "ztli lcaa noik drpy"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
